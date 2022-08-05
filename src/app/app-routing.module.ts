9558b22afef24a431a735b669680548e3ac5bb5b
izarsunshine7 <sunshineizar7@gmail.com>
2022-08-05 18:56:57 +0800

    Added carts and services page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,16 +16,20 @@ const routes: Routes = [
     loadChildren: () => import('./listing/listing.module').then( m => m.ListingPageModule)
   },
   {
-    path: 'cart',
-    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
-  },
-  {
     path: 'components',
     loadChildren: () => import('./components/components.module').then( m => m.ComponentsPageModule)
   },
   {
     path: 'explore-container',
     loadChildren: () => import('./explore-container/explore-container.module').then( m => m.ExploreContainerPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./pages/carts/carts.module').then( m => m.CartsPageModule)
+  },
+  {
+    path: 'services',
+    loadChildren: () => import('./services/services.module').then( m => m.ServicesPageModule)
   },
 
 ];
