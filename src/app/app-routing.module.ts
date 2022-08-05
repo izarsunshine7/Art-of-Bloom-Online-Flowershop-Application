6fa78b535e51a459e9173b1b3eaaa11b4f49f8e6
izarsunshine7 <sunshineizar7@gmail.com>
2022-08-05 19:33:30 +0800

    Added tab2 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
   {
     path: 'tab1',
     loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
   }
 ];
 @NgModule({
