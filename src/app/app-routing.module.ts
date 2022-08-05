9fb333366a5028fe3e500b0a4bca2374f67aa6f5
izarsunshine7 <sunshineizar7@gmail.com>
2022-08-05 19:09:03 +0800

    Updating app page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,41 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'listing',
-    loadChildren: () => import('./listing/listing.module').then( m => m.ListingPageModule)
-  },
-  {
-    path: 'components',
-    loadChildren: () => import('./components/components.module').then( m => m.ComponentsPageModule)
-  },
-  {
-    path: 'explore-container',
-    loadChildren: () => import('./explore-container/explore-container.module').then( m => m.ExploreContainerPageModule)
-  },
-  {
-    path: 'cart',
+    path: 'add-to-cart',
     loadChildren: () => import('./pages/carts/carts.module').then( m => m.CartsPageModule)
-  },
-  {
-    path: 'services',
-    loadChildren: () => import('./services/services.module').then( m => m.ServicesPageModule)
-  },
-
+  }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
